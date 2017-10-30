cae7bd303ae1b008e7c98b08003f9fb32c53cb46
Agsfer <agsfer@gmail.com>
2017-10-30 12:07:31 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,80 @@
+
+jQuery(document).ready(function( $ ) {
+  jQuery.fn.exists = function(){ return this.length > 0; }
+ 
+if ($('#responsive-menu-button').exists()) {
+     jQuery('#responsive-menu-button').sidr({
+        name: 'sidr-main',
+        source: '#nav'
+      });  
+}
+  jQuery('.overlay, .sidr-class-sidr-button-close').click(function(){ // when 'btnClose' is clicked…
+	    $.sidr('close', 'sidr-main');  // close the div with the ID of "sidr"
+	    return false;
+	});
+if ($('#user_login').exists()) {
+  jQuery('#user_login').sidr({
+      name: 'sidr-logo',
+      source: '.login_box',
+      side: 'right'
+    });
+}
+  jQuery('.overlay, .sidr-class-sidr-button-close').click(function(){ // when 'btnClose' is clicked…
+	    $.sidr('close', 'sidr-logo');  // close the div with the ID of "sidr"
+	    return false;
+	});
+  //Owl carousel
+
+
+});
+
+//BACKEND
+
+
+$( document ).ready(function() {
+
+ 
+ var panel1 = $('#slide-user').scotchPanel({
+    containerSelector: 'body',
+    direction: 'right',
+    duration: 300,
+    transition: 'ease',
+    clickSelector: '.user-head-box-mobile, #slide-user .close',
+    distanceX: '80%',
+    enableEscapeKey: true,
+    beforePanelOpen: function() {
+      // Reset all panels
+      $('.scotch-panel').css('z-index', 0);
+      // Bring current panel to top
+      $('#slide-user').css('z-index', 11);
+    },
+});
+var panel2 = $('#slide-notifications').scotchPanel({
+    containerSelector: 'body',
+    direction: 'right',
+    duration: 300,
+    transition: 'ease',
+    clickSelector: '.notif-mobile, #slide-notifications .close',
+    distanceX: '80%',
+    enableEscapeKey: true,
+    beforePanelOpen: function() {
+      $('.scotch-panel').css('z-index', 0);
+      $('#slide-notifications').css('z-index', 11);
+    }
+});
+
+$('.overlay').click(function() {
+  panel1.close();
+  panel2.close();
+  
+  return false;
+});
+
+});
+
+$(document).ready(function(){
+  //Select
+  jQuery('.selectpicker').selectpicker({  
+    
+  });
+});
